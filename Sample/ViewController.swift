2091d9ece99797912c4841f9d85f96ec2bc16e94
Paul Thomas <paulthomas2107@gmail.com>
2022-09-11 20:16:46 +0100

    Initial Commit

---
+++ Sample/ViewController.swift
@@ -0,0 +1,14 @@
+//___FILEHEADER___
+
+import UIKit
+
+class ___FILEBASENAME___: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
